2a3df98767898932eee80fdfd4d9ac7ceef7b3d3
thiago733 <thiago733@gmail.com>
2023-03-16 07:37:52 -0400

    Finalizando a questão 2

--- Questao2/Questao2.py
+++ Questao2/Questao2.py
@@ -1,3 +1,4 @@
+#exibindo o menu
 print('Bem vindo a lanchonete do Thiago Fortes')
 print('******************* Cardápio *******************')
 print('|Código|       Descrição                 |Valor|')
@@ -10,9 +11,11 @@ print('| 105  |       X-Tudo                    |17,00|')
 print('| 200  |       Refrigerante Lata         | 5,00|')
 print('| 201  |       Chá Gelado                | 4,00|')
 
+#iniciando variaveis
 subTotal = 0
 valorDoProduto = 0
 
+#fazendo os pedidos
 while True:
 
     codigoDoProduto = int(input('Entre com o código desejado: '))
@@ -32,43 +35,40 @@ while True:
           print(subTotal)
 
     elif  codigoDoProduto == 103:
-          valorDoProduto = 12.00
+          valorDoProduto = 13.00
           subTotal = valorDoProduto + subTotal
           print(subTotal)
 
     elif  codigoDoProduto == 104:
-          valorDoProduto = 12.00
+          valorDoProduto = 14.00
           subTotal = valorDoProduto + subTotal
           print(subTotal)
 
     elif  codigoDoProduto == 105:
-          valorDoProduto = 12.00
+          valorDoProduto = 17.00
           subTotal = valorDoProduto + subTotal
           print(subTotal)
 
     elif  codigoDoProduto == 200:
-          valorDoProduto = 12.00
+          valorDoProduto = 5.00
           subTotal = valorDoProduto + subTotal
           print(subTotal)
 
     elif  codigoDoProduto == 201:
-          valorDoProduto = 12.00
+          valorDoProduto = 4.00
           subTotal = valorDoProduto + subTotal
           print(subTotal)
 
     else:
         print('Opção inválida')
-
+#verficando se o cliente quer adicionar mais produtos
     print('Deseja pedir mais alguma coisa?')
     print('1 - Sim')
-    continuarPedido = input('0 - Não')
+    print('0 - Não')
+    continuarPedido = input()
     if  continuarPedido == '1':
         continue
+#exibindo o total da compra
     else:
         print('O total a ser pago é: R${:.2f}' .format(subTotal))
         break
-
-
-subTotal = valorDoProduto + subTotal
-print(subTotal)
-
